a48ab3ae7d0bcdf7cd0a3731eebd5aaddfadc7c8
Olexandr <minenkogt@gmail.com>
2023-12-19 20:54:10 +0200

    prettier added

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <h1>Hi</h1>
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <h1>Hi</h1>
+    </React.StrictMode>
+)
