2da3483d1aa30967f6deff8ba29f39973daaeb7f
Olexandr <minenkogt@gmail.com>
2024-01-08 16:58:59 +0200

    impoved react facebook

--- src/components/ReactFacebook/ReactFacebook.tsx
+++ src/components/ReactFacebook/ReactFacebook.tsx
@@ -1,5 +1,4 @@
 import { Typography } from '@mui/material'
-import { FacebookProvider, Like } from 'react-facebook'
 type Props = {}
 const ReactFacebook = (props: Props) => {
     return (
@@ -8,14 +7,6 @@ const ReactFacebook = (props: Props) => {
                 <Typography variant="h3" component="h3" className="title-h3">
                     LIKE US ON FACEBOOK
                 </Typography>
-                {/* <FacebookProvider appId="123456789">
-                    <Like
-                        href="https://www.facebook.com/PremiumCoding/?ref=embed_page"
-                        colorScheme="dark"
-                        showFaces
-                        share
-                    />
-                </FacebookProvider> */}
             </div>
         </div>
     )
