2828e11ff3fc2c69d15f68fa35b2dcafa564e2b4
Olexandr <minenkogt@gmail.com>
2024-01-08 17:02:32 +0200

    impoved app

--- src/layout/App.tsx
+++ src/layout/App.tsx
@@ -2,7 +2,7 @@ import Header from './Header'
 import Footer from './Footer'
 import CssBaseline from '@mui/material/CssBaseline'
 import { StyledEngineProvider } from '@mui/material/styles'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
 import Home from 'pages/Home/Home'
 import AboutUs from 'pages/AboutUs/AboutUs'
 import Lifestyle from 'pages/category/Lifestyle'
@@ -10,15 +10,8 @@ import EditorsChoise from 'pages/category/EditorsChoise'
 import Travel from 'pages/category/Travel'
 import Photography from 'pages/category/Photography'
 import FrontPageSlideshow from 'pages/category/FrontPageSlideshow'
-import ArticlesList from 'components/Pagination/Pagination'
 type Props = {}
 const App = (props: Props) => {
-    // const useQuery = () => {
-    //     return new URLSearchParams(useLocation().search)
-    // }
-
-    // const query = useQuery()
-    // const page = parseInt(query.get('page') || '1', 10) - 1
     return (
         <StyledEngineProvider injectFirst>
             <CssBaseline />
